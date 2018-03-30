debbef3bc65a1b320d825c149e4a069d3748a75e
adam n <adam.nawroc@gmail.com>
2018-03-30 18:13:34 +0000

    progress

--- app/models/figure.rb
+++ app/models/figure.rb
@@ -1,4 +1,7 @@
 class Figure < ActiveRecord::Base
   has_many :figures
 
+  # lf
+  # Now, use the has_many, belongs_to and has_many, :through macros to set up the correct associations in your models:
+
 end
