6bee516e86d72004c167d0a5c4e804c274c93354
adam n <adam.nawroc@gmail.com>
2018-04-01 10:49:32 +0000

    progress

--- app/models/figure.rb
+++ app/models/figure.rb
@@ -1,7 +1,13 @@
 class Figure < ActiveRecord::Base
-  has_many :figures
+  has_many :landmarks
+  has_many :figure_titles
+  has_many :titles, through: :figure_titles
 
   # lf
   # Now, use the has_many, belongs_to and has_many, :through macros to set up the correct associations in your models:
+
+  #
+
+
 
 end
